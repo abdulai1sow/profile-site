392c964bbe823e26269ef0d37fe91bc8cee08202
sowabdulai <abdulai-as@hotmail.com>
2020-04-18 18:42:02 -0400

    modile

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -21,8 +21,8 @@ export default class Index extends Component {
             <img className="hi-image" src="./me.jpg" alt="smile face" />
             <h2 className="con2-hi">Hi</h2>
             <p className="con2-para">
-              I'm a Software Engineer from Atlanta, Georgia. <br /> I have
-              passion for web development and <br />I love to create functional
+              I'm a Software Engineer from Atlanta, Georgia. I have
+              passion for web development and I love to create functional
               applications.
             </p>
           </div>
@@ -39,8 +39,7 @@ export default class Index extends Component {
               <h2 className="build-h2">Build what you want.</h2>
               <p className="build-para">
                 I like to keep it simple. My goals are to focus on
-                <br />
-                functionality, content and making <br />
+                functionality, content and making 
                 it easy for end users.
               </p>
             </div>
@@ -49,8 +48,8 @@ export default class Index extends Component {
               <img className="dev-pic" src="./2-pic-can.png" alt="" />
               <h2 className="header-dev">Develop what you need.</h2>
               <p className="dev-discription">
-                I'm a engineer, so i know how to create your applications <br />
-                to run across devices using the latest <br /> technologies
+                I'm a engineer, so i know how to create your applications 
+                to run across devices using the latest technologies
                 available.
               </p>
             </div>
@@ -61,7 +60,7 @@ export default class Index extends Component {
             <h1 className="con4-header">I can Help.</h1>
             <h3 className="con4-title">I'm currently available for work.</h3>
             <div className="con4-para">
-              If you are looking for my skills and knowledge, <br />
+              If you are looking for my skills and knowledge,
               or just want to say hey, then get in touch.
             </div>
             {/* <a href="mailto:abdulai-as@hotmail.com">
